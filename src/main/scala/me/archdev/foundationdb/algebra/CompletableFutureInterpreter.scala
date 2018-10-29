a319103130d207fff74cec0f9f644494a2140a0a
Arthur Kushka <arhelmus@gmail.com>
2018-10-29 13:30:46 +0100

    Refactored CompletableFutureInterpreter

--- src/main/scala/me/archdev/foundationdb/algebra/CompletableFutureInterpreter.scala
+++ src/main/scala/me/archdev/foundationdb/algebra/CompletableFutureInterpreter.scala
@@ -11,27 +11,36 @@ import me.archdev.foundationdb.serializers._
 object CompletableFutureInterpreter extends QueryAlgebra[TransactionPlan] {
 
   override def set[K: Tupler, V: Tupler](key: K, value: V)(implicit subspace: Subspace): TransactionPlan[Unit] =
-    StateT { tr =>
-      inCompletedFuture(tr, _.set(subspace.raw.pack(key.toTuple), value.toTuple.pack()))
+    transactionAction { tr =>
+      tr.set(subspace.raw.pack(key.toTuple), value.toTuple.pack())
+      null
     }
 
   override def get[K: Tupler, V: Tupler](key: K)(implicit subspace: Subspace): TransactionPlan[Option[V]] =
-    StateT { tr =>
-      tr.get(subspace.raw.pack(key.toTuple)).thenApply(output => tr -> parseFDBOutput[V](output))
+    transactionAction {
+      _.get(subspace.raw.pack(key.toTuple)).thenApply(output => parseFDBOutput[V](output))
     }
 
   override def delete[K: Tupler](key: K)(implicit subspace: Subspace): TransactionPlan[Unit] =
-    StateT(tr => inCompletedFuture(tr, _.clear(subspace.raw.pack(key.toTuple))))
+    transactionAction { tr =>
+      tr.clear(subspace.raw.pack(key.toTuple))
+      null
+    }
 
-  override def raw[V](f: Transaction => V): TransactionPlan[V] =
-    StateT(tr => inCompletedFuture(tr, f))
+  override def raw[V](f: Transaction => CompletableFuture[V]): TransactionPlan[V] =
+    transactionAction(f)
 
   private def parseFDBOutput[A](output: Array[Byte])(implicit vs: Tupler[A]): Option[A] =
     Option(output)
       .map(Tuple.fromBytes)
       .map(_.fromTuple[A])
 
-  private def inCompletedFuture[A](tr: Transaction, f: Transaction => A): CompletableFuture[(Transaction, A)] =
-    CompletableFuture.completedFuture(tr -> f(tr))
+  private def transactionAction[A](f: Transaction => CompletableFuture[A]): TransactionPlan[A] =
+    StateT { tr =>
+      Option(f(tr)) match {
+        case Some(result) => result.thenApply(result => tr -> result)
+        case None         => CompletableFuture.completedFuture(tr -> null.asInstanceOf[A])
+      }
+    }
 
 }
--- src/main/scala/me/archdev/foundationdb/algebra/package.scala
+++ src/main/scala/me/archdev/foundationdb/algebra/package.scala
@@ -1,5 +1,7 @@
 package me.archdev.foundationdb
 
+import java.util.concurrent.CompletableFuture
+
 import com.apple.foundationdb.Transaction
 import me.archdev.foundationdb.serializers.Tupler
 
@@ -9,7 +11,7 @@ package object algebra {
     def set[K: Tupler, V: Tupler](key: K, value: V)(implicit subspace: Subspace = Subspace()): F[Unit]
     def get[K: Tupler, V: Tupler](key: K)(implicit subspace: Subspace = Subspace()): F[Option[V]]
     def delete[K: Tupler](key: K)(implicit subspace: Subspace = Subspace()): F[Unit]
-    def raw[V](f: Transaction => V): F[V]
+    def raw[V](f: Transaction => CompletableFuture[V]): F[V]
   }
 
 }
